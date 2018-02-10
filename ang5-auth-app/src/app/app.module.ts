11c455b5c4799146dec4eee73ec1c26005cbf07b
tapsa <tapsa872@gmail.com>
2018-02-10 07:33:04 +0200

    Credentials and basic structure of the project

--- ang5-auth-app/src/app/app.module.ts
+++ ang5-auth-app/src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AngularFireModule } from "angularfire2";
+import { HttpModule } from "@angular/http";
+import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { EmailComponent } from './email/email.component';
+import { SignupComponent } from './signup/signup.component';
+import { MembersComponent } from './members/members.component';
 
+export const firebaseConfig = {
+  apiKey: "AIzaSyAJYdMiHWpHz0Vlyjnb3kKYjWVYSa-aAxc",
+  authDomain: "ng-auth-base.firebaseapp.com",
+  databaseURL: "https://ng-auth-base.firebaseio.com",
+  projectId: "ng-auth-base",
+  storageBucket: "ng-auth-base.appspot.com",
+  messagingSenderId: "649106629179"
+}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    EmailComponent,
+    SignupComponent,
+    MembersComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    FormsModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ auth.js
@@ -0,0 +1,16 @@
+{
+    apiKey: "AIzaSyAJYdMiHWpHz0Vlyjnb3kKYjWVYSa-aAxc",
+    authDomain: "ng-auth-base.firebaseapp.com",
+    databaseURL: "https://ng-auth-base.firebaseio.com",
+    projectId: "ng-auth-base",
+    storageBucket: "ng-auth-base.appspot.com",
+    messagingSenderId: "649106629179"
+  }
+  
+  
+  
+  
+  https://ng-auth-base.firebaseapp.com/__/auth/handler
+  
+  
+  
